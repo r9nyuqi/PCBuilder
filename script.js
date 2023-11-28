e36c949d32d4922d1b86db32927db5327b37721d
Wesley <randumbthingsxd@gmail.com>
2023-11-28 15:24:05 -0800

    forms

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const tabs = documnet.querySelectALl('[data-tab-target]')
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
-tabs.foreEach( tab => { 
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        target.classList.add('active')
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
     })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
 })
