3827a37322c82b9bfa89f6f360000149c4f115a8
Wesley <randumbthingsxd@gmail.com>
2023-12-09 23:57:31 -0800

    build a champion

--- PCBuilder/info.js
+++ PCBuilder/info.js
@@ -1,15 +1,19 @@
-// Get the video
+
 var video = document.getElementById("myVideo");
 
-// Get the button
+
 var btn = document.getElementById("myBtn");
 
-// Pause and play the video, and change the button text
-function video() {
-  if (video.paused) {
+
+function pauseVid() 
+{
+  if (video.paused) 
+  {
     video.play();
     btn.innerHTML = "Pause";
-  } else {
+  } 
+  else 
+  {
     video.pause();
     btn.innerHTML = "Play";
   }
